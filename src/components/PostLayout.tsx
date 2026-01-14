49e33ccd1abc408b0140551c12883f62dd8b05de
leonardkoch <leonard@leonardkoch.com>
2026-01-15 00:14:41 +0100

    Change dots background pattern

--- src/components/PostLayout.tsx
+++ src/components/PostLayout.tsx
@@ -1,5 +1,4 @@
 import { Link } from '@tanstack/react-router'
-import { DotPaperBackground } from './DotPaperBackground'
 import { TitleStack } from './TitleStack'
 import type { PostMeta } from '../posts'
 
@@ -20,44 +19,46 @@ function formatDate(dateString: string): string {
 
 export function PostLayout({ meta, children, isFullPage = false }: PostLayoutProps) {
   return (
-    <article className="w-full max-w-[1024px] mx-auto py-12">
-      {/* Full-width dot pattern container that extends beyond text */}
-      <div className="relative">
-        {/* Dot pattern extends beyond the content with extra horizontal space */}
-        <div className="absolute inset-0 -left-8 -right-8 md:-left-16 md:-right-16">
-          <DotPaperBackground color={meta.color} variant="title" />
-        </div>
-
-        {/* Title Area */}
-        <header className="relative z-10 pt-16 pb-12 px-4 md:px-8">
-          {isFullPage ? (
-            <div>
-              <TitleStack title={meta.title} color={meta.color} />
-            </div>
-          ) : (
-            <Link
-              to="/post/$slug"
-              params={{ slug: meta.slug }}
-              className="block hover:opacity-80 transition-opacity"
-            >
-              <TitleStack title={meta.title} color={meta.color} />
-            </Link>
-          )}
-          <time className="block mt-4 ml-[30px] text-gray-500 font-light text-sm">
-            {formatDate(meta.date)}
-          </time>
-        </header>
+    <article className="w-full max-w-[1024px] mx-auto py-12 relative">
+      {/* Header dot pattern - stronger intensity */}
+      <div className="absolute -left-8 -right-8 md:-left-16 md:-right-16 top-0 h-[220px]">
+        <div
+          className="absolute inset-0 dot-paper dot-paper-header"
+          style={{ '--dot-color': meta.color } as React.CSSProperties}
+        />
       </div>
 
-      {/* Content Area with faint continuous dots */}
-      <div className="relative">
-        {/* Dot pattern extends beyond the content */}
-        <div className="absolute inset-0 -left-8 -right-8 md:-left-16 md:-right-16">
-          <DotPaperBackground color={meta.color} variant="content" />
-        </div>
-        <div className="relative z-10 px-4 md:px-8 pb-8">
-          <div className="prose-blog ml-[30px]">{children}</div>
-        </div>
+      {/* Content dot pattern - faint with cutout around text */}
+      <div className="absolute -left-8 -right-8 md:-left-16 md:-right-16 top-[180px] bottom-0">
+        <div
+          className="absolute inset-0 dot-paper dot-paper-content"
+          style={{ '--dot-color': meta.color, backgroundPosition: '0 -180px' } as React.CSSProperties}
+        />
+      </div>
+
+      {/* Title Area */}
+      <header className="relative z-10 pt-16 pb-12 px-4 md:px-8">
+        {isFullPage ? (
+          <div>
+            <TitleStack title={meta.title} color={meta.color} />
+          </div>
+        ) : (
+          <Link
+            to="/post/$slug"
+            params={{ slug: meta.slug }}
+            className="block hover:opacity-80 transition-opacity"
+          >
+            <TitleStack title={meta.title} color={meta.color} />
+          </Link>
+        )}
+        <time className="block mt-4 ml-[30px] text-gray-500 font-light text-sm">
+          {formatDate(meta.date)}
+        </time>
+      </header>
+
+      {/* Content Area */}
+      <div className="relative z-10 px-4 md:px-8 pb-8">
+        <div className="prose-blog ml-[30px]">{children}</div>
       </div>
     </article>
   )
